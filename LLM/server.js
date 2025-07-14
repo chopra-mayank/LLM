677c8b5ffafb3ad1ae0bc388d084d7d0e56518f3
Mayank Chopra <mayank.chopra2312@gmail.com>
2025-07-14 13:22:15 +0530

    Updated

--- LLM/server.js
+++ LLM/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use("/api/itinerary", itineraryRoute);
 app.use("/api/tavily", activity);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   
